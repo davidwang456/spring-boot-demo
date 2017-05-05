9de1affa7192855ad1e1396f49603ee30a5ac840
davidwang456 <davidwang456@sina.com>
2017-05-05 13:06:20 +0800

    增加map的测试

--- spring-boot-redis/src/main/java/com/davidwang456/controller/TestController.java
+++ spring-boot-redis/src/main/java/com/davidwang456/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.davidwang456.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +22,7 @@ public class TestController {
   
   @RequestMapping(value="/test")
   @ResponseBody
-  public String test(String userName) {
+  public String testKey(String userName) {
 		// 保存对象
 		User user = new User("ss", 20);
 		redisTemplate.opsForValue().set(user.getUsername(), user);
@@ -30,5 +34,29 @@ public class TestController {
 		redisTemplate.opsForValue().set(user.getUsername(), user);
 		User uu=redisTemplate.opsForValue().get(userName);
 		return uu.getUsername()+","+uu.getAge();
+  }
+  
+  @RequestMapping(value="/testmap")
+  @ResponseBody
+  public String testmap(String userName) {
+	  HashOperations<String, Object, Object> hash=redisTemplate.opsForHash();
+	  
+      Map<String,Object> map = new HashMap<String,Object>();
+      map.put("name", "dd");
+      map.put("age", "26");
+      hash.putAll("lpMap", map);
+      Map<Object,Object> old=hash.entries("lpMap"); 
+      StringBuffer sbf=new StringBuffer();
+      for(Object obj:old.keySet()){
+    	  sbf.append("key:"+obj+",value:"+old.get(obj)).append("\n");
+      }
+      hash.put("lpMap", "name", "ww");
+      Map<Object,Object> last=hash.entries("lpMap"); 
+      
+      StringBuffer sb=new StringBuffer();
+      for(Object obj:last.keySet()){
+    	  sb.append("key:"+obj+",value:"+last.get(obj)).append("\n");
+      }
+      return "old key-value="+sbf.toString()+",last key-value="+sb.toString();
   }
 }
